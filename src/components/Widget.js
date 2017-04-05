a63d9f157c981fd0fbb69664868ac85eb7886fe5
Alex Sinfarosa <as898@cornell.edu>
2017-04-05 16:13:41 -0400

    updates

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -7,13 +7,13 @@ import * as d3 from "d3";
 export default class Widget extends Component {
   render() {
     // console.log(d3);
-    const width = 300;
-    const height = 300;
+    const width = 500;
+    const height = 500;
     const margin = {
-      top: 60,
+      top: 160,
       right: 40,
       bottom: 30,
-      left: 40
+      left: 340
     };
 
     const config = {
@@ -21,55 +21,48 @@ export default class Widget extends Component {
       maxAngle: 90,
       innerTickRingOffset: -30,
       innerTickCounterclockSpin: 0,
-      innerTickNumber: 10,
+      innerTickNumber: 24,
       outerTickRingOffset: 25,
       outerTickCounterclockSpin: 5,
       outerTickBorderLength: 30
     };
 
     const radius = Math.min(width, height) / 2;
+    const percentToDeg = percent => percent * 180 / 60;
 
-    const percentToDeg = percent => percent * 180 / 100;
-
-    // const element = d3.select(".gauge");
-
-    const arc = d3
+    const currentYear = d3
       .arc()
       .innerRadius(radius)
-      .outerRadius(radius - 10)
+      .outerRadius(radius + 20)
       .startAngle(-90 * (Math.PI / 180))
       .endAngle(90 * (Math.PI / 180));
 
-    const scale = d3.scaleLinear().domain([0, 10]).range([0, 1]);
+    const p20402069 = d3
+      .arc()
+      .innerRadius(radius + 25)
+      .outerRadius(radius + 45)
+      .startAngle(-90 * (Math.PI / 180))
+      .endAngle(90 * (Math.PI / 180));
+
+    const p20692099 = d3
+      .arc()
+      .innerRadius(radius + 50)
+      .outerRadius(radius + 70)
+      .startAngle(-90 * (Math.PI / 180))
+      .endAngle(90 * (Math.PI / 180));
+
+    const scale = d3.scaleLinear().domain([0, 60]).range([0, 1]);
 
     const innerTicks = scale.ticks(config.innerTickNumber).map(tick => ({
       value: tick,
       label: tick
     }));
-    console.log(innerTicks.map(e => e.value));
-    // const innerTicksGroup = chart.append("g").attr("class", "inner-ticks");
-    //
-    // innerTicksGroup
-    //   .selectAll("text")
-    //   .data(innerTicks)
-    //   .enter()
-    //   .append("text")
-    //   .attr("class", "tick")
-    //   .attr("text-anchor", "middle")
-    //   .attr("transform", function(d) {
-    //     var ratio = scale(d.value);
-    //     var newAngle = config.minAngle + ratio * 180;
-    //     return `rotate(${newAngle}) translate(0, ${-(radius + config.innerTickRingOffset)})`;
-    //   })
-    //   .text(d => d.label);
 
     const rotate = d => {
       const ratio = scale(d.value);
       const newAngle = config.minAngle + ratio * 180;
       return `rotate(${newAngle}) translate(0, ${-(radius + config.innerTickRingOffset)})`;
     };
-
-    innerTicks.map(e => console.log(e.label));
 
     return (
       <svg width="100%" height={`${height + margin.top + margin.bottom}`}>
@@ -78,22 +71,25 @@ export default class Widget extends Component {
             `translate(${width / 2 + margin.left}, ${height / 2 + margin.top})`
           }
         >
-          <path d={arc()} />
-          <circle cx={0} cy={0} r={5} />
+          <path d={currentYear()} fill="red" />
+          <path d={p20402069()} fill="lightgreen" />
+          <path d={p20692099()} fill="orange" />
+          <circle cx={0} cy={0} r={7} />
+          <text textAnchor="middle" x={0} y={40}>{49}</text>
           <line
             stroke="#aaa"
             strokeWidth={2}
             x1={0}
             y1={0}
-            x2={-160}
+            x2={-210}
             y2={0}
-            transform={`rotate(${percentToDeg(40)})`}
+            transform={`rotate(${percentToDeg(17)})`}
           />
           <g>
             {innerTicks.map((e, i) => {
               return (
                 <text
-                  style={{ fill: "#333", fontSize: "9px" }}
+                  style={{ fill: "#333", fontSize: "11px" }}
                   textAnchor="middle"
                   key={i}
                   transform={rotate(e)}

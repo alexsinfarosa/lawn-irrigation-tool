59fb047426602aac525ad3660193f9bac7c5dbea
alexsinfarosa <as898@cornell.edu>
2017-04-10 00:04:32 -0400

    updates

--- src/components/WidgetTest.js
+++ src/components/WidgetTest.js
@@ -7,46 +7,7 @@ import * as d3 from "d3";
 export default class Widget extends Component {
   render() {
     const { temperature } = this.props.store.app;
-    const r = Math.PI / 180;
-    const data = [
-      {
-        label: "A",
-        startAngle: -90 * r,
-        endAngle: -90 + 0.2 * r,
-        color: "#BBDEFB"
-      },
-      {
-        label: "B",
-        startAngle: -90 * r + 0.2,
-        endAngle: -90 * r + 0.4,
-        color: "#FFF9C4"
-      },
-      {
-        label: "C",
-        startAngle: -90 * r + 0.4,
-        endAngle: -90 * r + 0.6,
-        color: "#FFE0B2"
-      },
-      {
-        label: "D",
-        startAngle: -90 * r + 0.6,
-        endAngle: -90 * r + 0.8,
-        color: "#ffcdd2"
-      },
-      {
-        label: "E",
-        startAngle: -90 * r + 0.8,
-        endAngle: -90 * r + 1,
-        color: "#eee"
-      }
-    ];
-
     const acis = [10, 20, 40, 60];
-    const color = d3
-      .scaleOrdinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#3458"]);
-    console.log(color(acis));
-
     const margin = {
       top: 50,
       right: 10,
@@ -57,16 +18,15 @@ export default class Widget extends Component {
     const width = 700 - margin.left - margin.right;
     const height = 400;
     const radius = Math.min(width, height) / 2;
+    const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
     // Returns a path data string
-    const currentYear = d3.arc().innerRadius(radius - 30).outerRadius(radius);
+    const currentYear = d3.arc().innerRadius(radius).outerRadius(radius + 20);
     const pieChart = d3
       .pie()
       .sort(null)
       .startAngle(-0.5 * Math.PI)
       .endAngle(0.5 * Math.PI);
-
-    const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
     const graph = pieChart(acis).map((e, i) => {
       console.log(e);
@@ -121,7 +81,7 @@ export default class Widget extends Component {
             strokeWidth={2}
             x1={0}
             y1={0}
-            x2={-205}
+            x2={-170}
             y2={0}
             transform={`rotate(${percentToDeg(temperature)})`}
           />

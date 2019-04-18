d742366fbeda5ee57eebb5faba681a1f6fb19337
alexsinfarosa <as898@cornell.edu>
2019-04-18 18:14:08 -0400

    styling index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,27 +16,35 @@ export default function App() {
   if (loading) return <Loading />
 
   return (
-    <Container maxWidth="sm">
-      <Box my={4} display="flex" flexDirection="column" alignItems="center">
+    <Container maxWidth="sm" style={{ padding: 0 }}>
+      <Box
+        my={4}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        bgcolor="pink"
+      >
         <CompaniesLogos />
 
-        <Box width={window.innerWidth < 480 ? "42%" : "30%"} mb={4}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Lawn
-          </Typography>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Irrigation
-          </Typography>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Tool
-          </Typography>
+        <Box display="flex" justifyContent="center" mb={5}>
+          <Box>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Lawn...
+            </Typography>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Irrigation
+            </Typography>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Tool
+            </Typography>
+          </Box>
         </Box>
 
         <Box mb={4}>
           <Typography color="error">- ßeta Release -</Typography>
         </Box>
 
-        <Box bgcolor="primary.main" mx={-2} mb={8} p={2} px={4} color="white">
+        <Box bgcolor="primary.main" mb={8} p={2} px={4} color="white">
           <Typography align="center">
             You can find out your watering needs for today and the next two days
           </Typography>

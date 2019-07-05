d65c737f8dba9e579eaa60f6af9715a1890fe111
alexsinfarosa <as898@cornell.edu>
2019-07-05 22:43:01 +0200

    Updates

--- sw.js
+++ sw.js
@@ -29,14 +29,14 @@ self.__precacheManifest = [
     "url": "webpack-runtime-5fcc634fac6d429383ae.js"
   },
   {
-    "url": "app-0459b47ef2558c188b23.js"
+    "url": "app-ad504c5743d12ab355b5.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-20adaf7ca37d9c960f56.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "30b88a51d081e486a23e54c2ab9c3ee5"
+    "revision": "711ec87ab18ea5c9a7d6e6c4d55b1146"
   },
   {
     "url": "styles.41b5d7d3413cddbae1f1.css"
